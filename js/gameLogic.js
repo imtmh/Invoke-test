b6ac7009ff5f9f016ed55bb836243e9e1211d32e
Thirumalesh Chinna <tmhs.bits@gmail.com>
2016-04-07 16:19:03 +0530

    update content

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,50 +1,50 @@
-         $(document).ready(function(){
-		 	 $(function() {
-			    $( "#snaptarget" ).sortable({
-					revert: true,
-					cursor: "move",
-					containment:"parent",
-					stop: function (){
-						var result = true;
-						var position = [1,2,3,4,5,6,7,8,9];
-						$.each($('div'), function(index, element){
-							var temp=$(element).attr('id');
-							var temp2='draggable'+position[index]+'';
-							if(temp != temp2){
-								result = false;
-								return false;
-							};
-						});
-						if(result){
-							$("#result").text('Done!');
-							$("#result").css("color", "blue");
-						} else {
-							$("#result").text('');
-							$("#result").css("color", "red");
-						}
-					}
-				});
-			});
-         
-        Array.prototype.shuffle = function () {
-                       var n = this.length;
-                       while (n--) {
-                               var i = Math.floor(n * Math.random());
-                               var tmp = this[i];
-                               this[i] = this[n];
-                               this[n] = tmp;
-                       }
-                       return this;
-           };
-           
-			 $('#randmizeButton').click(function(){
-				 var sortedArray = [1,2,3,4,5,6,7,8,9];
-				 sortedArray.shuffle();
-				 var html = '';//'<section id="snaptarget" >';
-				 $.each(sortedArray, function(index, position){
-					html+= '<div id="draggable'+position+'" class="draggable ui-widget-content"><p>'+position+'</p></div>';
-				 });
-					$('#snaptarget').html(html);
-			 });
-			 $('button').trigger('click');
-         });
+$(document).ready(function(){
+	function handleSortableStop(a, b) {
+		var result = $.grep($('div'), isOutOfOrder);
+		if(result==null || result.length==0){
+			$("#result").text('Done!');
+			$("#result").css("color", "blue");
+		} else {
+			$("#result").text('not yet done');
+			$("#result").css("color", "red");
+		}
+	}
+	function isOutOfOrder(element, index){
+		var sortedArray = [1,2,3,4,5,6,7,8,9];
+		var elementId=$(element).attr('id');
+		var SortedElementId='draggable'+sortedArray[index]+'';
+		var bool = elementId!=SortedElementId;
+		return bool;
+	}
+	function shuffleArray(arr) {
+	   var n = arr.length;
+	   while (n--) {
+			   var i = Math.floor(n * Math.random());
+			   var tmp = arr[i];
+			   arr[i] = arr[n];
+			   arr[n] = tmp;
+	   }
+	   return arr;
+	};
+	function renderContent(){
+		var sortedArray = [1,2,3,4,5,6,7,8,9];
+		var shuffledArray = shuffleArray(sortedArray);
+		var sectionContent = '';
+		$.each(shuffledArray, function(index, position){
+			sectionContent+= '<div id="draggable'+position+'" class="draggable ui-widget-content"><p>'+position+'</p></div>';
+		});
+		$('#sectionId').html(sectionContent);
+	}
+	$('#randmizeButton').click(renderContent);
+	$(function() {
+		$( "#sectionId" ).sortable({
+			revert: true,
+			cursor: "move",
+			containment:"parent",
+			stop: handleSortableStop
+		});
+	});
+	
+	//call renderContent to get the page loaded for the first time
+	renderContent();
+});
