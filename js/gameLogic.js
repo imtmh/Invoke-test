67883ec3a5eb3d5dae08b85af16df3ae647c992a
Thirumalesh Chinna <tmhs.bits@gmail.com>
2016-04-11 12:27:40 +0530

    Update gameLogic.js

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,37 +1,39 @@
 $(document).ready(function(){
-	function handleSortableStop(a, b) {
+	function handleSortableStop(event, ui) {
 		var result = $.grep($('div'), isOutOfOrder);
-		if(result==null || result.length==0){
+		if(result === null || result.length == 0){
 			$("#result").text('Done!');
 			$("#result").css("color", "blue");
 		} else {
-			$("#result").text('not yet done');
+			$("#result").text('');
 			$("#result").css("color", "red");
 		}
 	}
+	//to check if the element and index are in order
 	function isOutOfOrder(element, index){
-		var sortedArray = [1,2,3,4,5,6,7,8,9];
-		var elementId=$(element).attr('id');
-		var SortedElementId='draggable'+sortedArray[index]+'';
-		var bool = elementId!=SortedElementId;
+		var sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+		var elementId = $(element).attr('id');
+		var SortedElementId = 'draggable'+sortedArray[index]+'';
+		var bool = (elementId != SortedElementId);
 		return bool;
 	}
+	//to shuffle the array
 	function shuffleArray(arr) {
-	   var n = arr.length;
-	   while (n--) {
-			   var i = Math.floor(n * Math.random());
-			   var tmp = arr[i];
-			   arr[i] = arr[n];
-			   arr[n] = tmp;
+	   var arrayLength = arr.length;
+	   while (arrayLength--) {
+		   var i = Math.floor(arrayLength * Math.random());
+		   var swapTemp = arr[i];
+		   arr[i] = arr[arrayLength];
+		   arr[arrayLength] = swapTemp;
 	   }
 	   return arr;
 	};
 	function renderContent(){
-		var sortedArray = [1,2,3,4,5,6,7,8,9];
+		var sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 		var shuffledArray = shuffleArray(sortedArray);
 		var sectionContent = '';
 		$.each(shuffledArray, function(index, position){
-			sectionContent+= '<div id="draggable'+position+'" class="draggable ui-widget-content"><p>'+position+'</p></div>';
+			sectionContent += '<div id="draggable' + position + '" class="draggable ui-widget-content"><p>' + position + '</p></div>';
 		});
 		$('#sectionId').html(sectionContent);
 	}
@@ -40,7 +42,7 @@ $(document).ready(function(){
 		$( "#sectionId" ).sortable({
 			revert: true,
 			cursor: "move",
-			containment:"parent",
+			//containment:"parent",
 			stop: handleSortableStop
 		});
 	});
