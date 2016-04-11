d9085d37e709acf2cc4a0d1f82946468f03ed599
Thirumalesh Chinna <tmhs.bits@gmail.com>
2016-04-11 12:49:25 +0530

    test

---
+++ app.js
@@ -0,0 +1,19 @@
+
+console.log(a);
+console.log(b);
+
+function b(){
+    var v = 2;
+    a();
+}
+b();
+console.log(d);
+var d = 23;
+console.log(d);
+function a(){
+    
+    console.log("v"+v);
+var d = 20;
+console.log(d);
+    
+}
