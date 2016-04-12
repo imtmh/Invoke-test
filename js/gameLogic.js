f5089e9a379b7917b24e72d68aecefdae574db0c
Thirumalesh Chinna <tmhs.bits@gmail.com>
2016-04-12 18:40:22 +0530

    final copy

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,45 +1,45 @@
-$(document).ready(function(){
-	function handleSortableStop(event, ui) {
-		var result = $.grep($('div'), isOutOfOrder);
-		if(result === null || result.length == 0){
-			$("#result").text('Done!');
-			$("#result").css("color", "blue");
-		} else {
-			$("#result").text('');
-			$("#result").css("color", "red");
-		}
-	}
+$(document).ready(function () {
 	//to check if the element and index are in order
-	function isOutOfOrder(element, index){
+	function isOutOfOrder(element, index) {
 		var sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 		var elementId = $(element).attr('id');
-		var SortedElementId = 'draggable'+sortedArray[index]+'';
-		var bool = (elementId != SortedElementId);
-		return bool;
+		var SortedElementId = 'draggable' + sortedArray[index];
+		return (elementId !== SortedElementId);
 	}
+    function handleSortableStop(event, ui) {
+		var result = $.grep($(".draggable"), isOutOfOrder);
+		//console.log($(".draggable"));
+		if (result === null || result.length === 0) {
+			$("#result").text('Done!');
+		} else {
+			$("#result").text(result.length + ' more bricks');
+		}
+	}
+
 	//to shuffle the array
 	function shuffleArray(arr) {
-	   var arrayLength = arr.length;
-	   while (arrayLength--) {
-		   var i = Math.floor(arrayLength * Math.random());
-		   var swapTemp = arr[i];
-		   arr[i] = arr[arrayLength];
-		   arr[arrayLength] = swapTemp;
-	   }
-	   return arr;
-	};
-	function renderContent(){
+        var arrayLength = arr.length;
+        while (arrayLength--) {
+            var i = Math.floor(arrayLength * Math.random());
+            var swapTemp = arr[i];
+            arr[i] = arr[arrayLength];
+            arr[arrayLength] = swapTemp;
+        }
+        return arr;
+	}
+	function renderContent() {
 		var sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 		var shuffledArray = shuffleArray(sortedArray);
 		var sectionContent = '';
-		$.each(shuffledArray, function(index, position){
-			sectionContent += '<div id="draggable' + position + '" class="draggable ui-widget-content"><p>' + position + '</p></div>';
+		$.each(shuffledArray, function (index, position) {
+			sectionContent += '<div id="draggable' + position + '" class="draggable  green "><p>' + position + '</p></div>';
 		});
 		$('#sectionId').html(sectionContent);
+		$("#result").text('');
 	}
 	$('#randmizeButton').click(renderContent);
-	$(function() {
-		$( "#sectionId" ).sortable({
+	$(function () {
+		$("#sectionId").sortable({
 			revert: true,
 			cursor: "move",
 			//containment:"parent",
